e4ba126b18444f0d3fcbc460f0ee40a906c21736
Steve McMahon <steve@reidmcmahon.com>
2009-08-07 21:28:47 +0000

    Import jqtools integration package skeleton. refs #9250
    svn path=/plone.app.jqtools/trunk/; revision=28504

---
+++ plone/app/jqtools/tests/testsDocTests.py
@@ -0,0 +1,35 @@
+import unittest
+import doctest
+
+from zope.testing import doctestunit
+from zope.component import testing
+from Testing import ZopeTestCase as ztc
+
+from Products.Five import zcml
+from Products.Five import fiveconfigure
+from Products.Five.testbrowser import Browser
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import onsetup
+
+
+testfiles = (
+    'ploneIntegration.txt',
+)
+
+ptc.setupPloneSite()
+
+
+def test_suite():
+    return unittest.TestSuite([
+
+        ztc.FunctionalDocFileSuite(
+            f, package = 'plone.app.jqtools.tests',
+            test_class = ptc.FunctionalTestCase,
+            optionflags = doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS)
+        
+            for f in testfiles
+        ])
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0b1'
+
+setup(name='plone.app.jqtools',
+      version=version,
+      description="JQuery Tools Integration for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+          "Framework :: Plone",
+          "Programming Language :: Python",
+        ],
+      keywords='Plone JQuery',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://svn.plone.org/svn/plone/plone.app.jqtools',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.CMFCore',
+          'Products.GenericSetup',
+      ],
+      )
