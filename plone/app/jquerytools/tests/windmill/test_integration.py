49c1fb24fbc5969824e47624791aa4cc5883d06a
Steve McMahon <steve@reidmcmahon.com>
2010-06-17 00:14:27 +0000

    Comment the login form test
    svn path=/plone.app.jquerytools/trunk/; revision=37324

--- plone/app/jquerytools/tests/windmill/test_integration.py
+++ plone/app/jquerytools/tests/windmill/test_integration.py
@@ -17,6 +17,10 @@ class IntegrationTestCaseAnon(WindmillTestCase):
         self.login_user()
 
     def test_login_popup(self):
+        """Check that login form works as popup; if it does, this confirms
+           overlays work for unauth users and that the refresh no-form
+           action works.
+        """
         client = self.wm
 
         # Log out so that we can test log in
@@ -25,30 +29,34 @@ class IntegrationTestCaseAnon(WindmillTestCase):
         client.click(link=u'Home')
         client.waits.forPageLoad(timeout=u'20000')
 
+        # Test that login option is set as an overlay
         client.asserts.assertNode(id=u'anon-personalbar')
         client.asserts.assertNode(jquery=u'("#anon-personalbar a.link-overlay")[0]')
+        # click on it, look for overlay
         client.click(jquery=u'("#anon-personalbar a.link-overlay")[0]')
         client.waits.forElement(jquery=u'("div.pb-ajax #login_form")[0]', timeout=u'1000')
+        # make sure that clicking outside dismisses popup
         client.click(id=u'exposeMask')
         client.waits.sleep(milliseconds=u'500')
         client.asserts.assertNotNode(jquery=u'("div.pb-ajax #login_form")[0]')
+        
+        # open popup again, try submitting empty; look for error.
+        # presence of error confirms that we're not losing the portal message
+        # in the popup form contents filter.
         client.asserts.assertNode(id=u'anon-personalbar')
         client.asserts.assertNode(jquery=u'("#anon-personalbar a.link-overlay")[0]')
         client.click(jquery=u'("#anon-personalbar a.link-overlay")[0]')
         client.waits.forElement(jquery=u'("div.pb-ajax #login_form")[0]', timeout=u'1000')
         client.click(name=u'submit')
         client.waits.forElement(jquery=u"('div.pb-ajax dl .portalMessage .error dt')", timeout=u'1000')
+        
+        # try again with real login
         client.type(text=u'portal_owner', id=u'__ac_name')
         client.type(text=u'secret', id=u'__ac_password')
         client.click(name=u'submit')
+        # expect a page load
         client.waits.forPageLoad(timeout=u'20000')
         client.asserts.assertNode(id=u'portal-personaltools')
-        # client.click(link=u'Log out')
-        # client.waits.forPageLoad(timeout=u'20000')
-        # self.setRoles(['Manager'])
         
-        # log back in for other tests
-        # self.login_user()
-
 def test_suite():
     return unittest.defaultTestLoader.loadTestsFromName(__name__)
