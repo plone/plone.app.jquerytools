9a5a7fd798934d9e83d80df4eb77ecc8308eb781
David Glick <davidglick@groundwire.org>
2010-07-17 23:49:04 +0000

    add overlay styles for Plone 3
    svn path=/plone.app.jquerytools/trunk/; revision=37960

---
+++ plone/app/jquerytools/setuphandlers.py
@@ -0,0 +1,22 @@
+from Products.CMFCore.utils import getToolByName
+try:
+    from plone.app.upgrade import v40
+    HAS_PLONE40 = True
+except ImportError:
+    HAS_PLONE40 = False
+
+def disable_css(context):
+    cssreg = getToolByName(context, 'portal_css')
+    res = cssreg.getResource('++resource++plone.app.jquerytools.overlays.css')
+    if res is not None:
+        res.setEnabled(False)
+
+def importVarious(gscontext):
+    # don't run as a step for other profiles
+    if gscontext.readDataFile('is_jquerytools_profile.txt') is None:
+        return
+    
+    site = gscontext.getSite()
+    if HAS_PLONE40:
+        # in Plone 4, we don't need overlays.css
+        disable_css(site)
---
+++ plone/app/jquerytools/upgrades/__init__.py
@@ -0,0 +1,13 @@
+from Products.CMFCore.utils import getToolByName
+
+def null_upgrade_step(context):
+    pass
+
+def upgrade1to2(context):
+    # run the custom import step (and cssregistry dependency) to install
+    # overlays.css on Plone 3
+    context = getToolByName(context, "portal_setup")
+    context.runAllImportStepFromProfile(
+        'profile-plone.app.jquerytools:default',
+        'jquerytools-various',
+        purge_old=False)
