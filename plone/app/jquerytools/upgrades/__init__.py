08c4149a0066f6a0a133553f0969b11ff5c6b247
David Glick <davidglick@groundwire.org>
2010-07-20 00:55:09 +0000

    fix a few more upgrade issues
    svn path=/plone.app.jquerytools/trunk/; revision=38155

--- plone/app/jquerytools/upgrades/__init__.py
+++ plone/app/jquerytools/upgrades/__init__.py
@@ -7,7 +7,7 @@ def upgrade1to2(context):
     # run the custom import step (and cssregistry dependency) to install
     # overlays.css on Plone 3
     context = getToolByName(context, "portal_setup")
-    context.runAllImportStepFromProfile(
+    context.runImportStepFromProfile(
         'profile-plone.app.jquerytools:default',
         'jquerytools-various',
         purge_old=False)
