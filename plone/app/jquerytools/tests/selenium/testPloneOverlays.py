0c2c3f5e059c91a411a517ce6dc7b5cd1fa14185
Steve McMahon <steve@reidmcmahon.com>
2011-01-01 22:16:36 +0000

    Expand login overlay test to cover bad login error
    svn path=/plone.app.jquerytools/trunk/; revision=46560

--- plone/app/jquerytools/tests/selenium/testPloneOverlays.py
+++ plone/app/jquerytools/tests/selenium/testPloneOverlays.py
@@ -11,18 +11,34 @@ class OverlayTestCase(SeleniumTestCase):
         self.wait()
         sel = self.selenium
 
+        # test that click on log in opens overlay
         sel.click("personaltools-login")
         self.waitForElement('form#login_form')
-        self.failUnless(sel.is_element_present("css=div.overlay.overlay-ajax form#login_form"))
+        self.failUnless(sel.is_element_present("css=div.overlay-ajax form#login_form"))
+        
+        # clicking anywhere else should close overlay
         sel.click("exposeMask")
         time.sleep(0.5)
         self.failIf(sel.is_element_present("id=login_form"))
-      
+
+        # reload overlay
         sel.click("personaltools-login")
-        self.waitForElement('form#login_form')
+        self.waitForElement("#login-form")
+        
+        # click on empty form submit and look for error
+        # in overlay
+        sel.click("submit")
+        self.waitForElement("dl.portalMessage.error")
+        self.failUnless(sel.is_element_present("css=div.overlay.overlay-ajax form#login_form"))
+        self.failUnless(sel.is_text_present("Error"))
+
+        # Now, try a real login
         sel.type("name=__ac_name", TEST_USER_NAME)
         sel.type("name=__ac_password", TEST_USER_PASSWORD)
         sel.click("submit")
         self.wait()
-        self.failUnless(sel.is_text_present("Log out"))
+        # overlay should be gone
+        self.failIf(sel.is_element_present("css=div.overlay-ajax form#login_form"))
+        self.failUnless(sel.is_element_present("id=personaltools-logout"))
+        
 
