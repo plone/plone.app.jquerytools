76135243f53ce2546133de12340a503984556ea4
tisto <tisto@plone.org>
2013-10-10 21:48:41 +0200

    Replace deprecated test assert statements.

--- plone/app/jquerytools/tests/selenium/testPloneOverlays.py
+++ plone/app/jquerytools/tests/selenium/testPloneOverlays.py
@@ -13,36 +13,36 @@ from plone.app.testing.interfaces import TEST_USER_ID
 # thus catches the DOM in an changing state?
 
 class OverlayTestCase(SeleniumTestCase):
-        
+
     def test_login_overlay(self):
-        
+
         sel = self.selenium
 
         self.open("/")
-        
+
         # test that click on log-in opens overlay
         sel.click("id=personaltools-login")
         self.waitForElement('form#login_form')
         self.waitForElement("div.overlay-ajax form#login_form")
         self.waitForElement("#exposeMask")
         time.sleep(0.5)
-        
+
         # clicking anywhere else should close overlay
         sel.click("id=exposeMask")
         time.sleep(1)
-        self.failIf(sel.is_element_present("id=login_form"))
+        self.assertFalse(sel.is_element_present("id=login_form"))
 
         # reload overlay
         sel.click("id=personaltools-login")
         self.waitForElement("#login-form")
-        
+
         # click on empty form submit and look for error
         # in overlay
         sel.click("submit")
         self.waitForElement("dl.portalMessage.error")
         self.waitForElement("div.overlay-ajax form#login_form")
         time.sleep(0.5)
-        self.failUnless(sel.is_text_present("Error"))
+        self.assertTrue(sel.is_text_present("Error"))
 
         # Now, try a real login
         sel.type("name=__ac_name", TEST_USER_NAME)
@@ -52,7 +52,7 @@ class OverlayTestCase(SeleniumTestCase):
         self.waitForElement("#personaltools-logout")
         time.sleep(0.5)
         # overlay should be gone
-        self.failIf(sel.is_element_present("css=div.overlay-ajax form#login_form"))
+        self.assertFalse(sel.is_element_present("css=div.overlay-ajax form#login_form"))
 
         self.open("logout")
         time.sleep(0.5)
@@ -80,23 +80,23 @@ class OverlayTestCase(SeleniumTestCase):
 
         sel.click("link=f1")
         self.wait()
-        
+
         sel.click("id=delete")
         self.waitForElement("div.overlay-ajax form#delete_confirmation")
         self.waitForElement("#exposeMask")
         self.waitForElement("div.overlay-ajax form#delete_confirmation form.button.Cancel")
         sel.click("form.button.Cancel")
-                
+
         sel.click("id=delete")
         self.waitForElement("div.overlay-ajax form#delete_confirmation form.button.Cancel")
         sel.click("//input[@value='Delete']")
 
         self.wait()
         time.sleep(0.5)
-        
-        self.failUnless(sel.get_location().rstrip('/').split('/')[-1] == PLONE_SITE_ID)
 
-        self.failIf(sel.is_element_present("id=portaltab-f1"))
-        
+        self.assertTrue(sel.get_location().rstrip('/').split('/')[-1] == PLONE_SITE_ID)
+
+        self.assertFalse(sel.is_element_present("id=portaltab-f1"))
+
         self.open("logout")
         time.sleep(0.5)
--- plone/app/jquerytools/tests/selenium/testXForms.py
+++ plone/app/jquerytools/tests/selenium/testXForms.py
@@ -5,7 +5,7 @@ from base import SeleniumTestCase
 from plone.app.testing import TEST_USER_NAME, TEST_USER_PASSWORD, TEST_USER_ROLES
 
 class FormTestCase(SeleniumTestCase):
-        
+
     def test_afancy_form(self):
         """ test basic form functions on a complex form """
 
@@ -16,9 +16,9 @@ class FormTestCase(SeleniumTestCase):
         sel.click("taform")
         self.waitForElement("div.overlay-ajax form")
         time.sleep(1)
-        self.failUnless(sel.is_text_present("Test Form"))
-        self.failIf(sel.is_text_present("exact:Should not show"))        
-        self.failUnless(sel.is_text_present("exact:ajax_load:"))
+        self.assertTrue(sel.is_text_present("Test Form"))
+        self.assertFalse(sel.is_text_present("exact:Should not show"))
+        self.assertTrue(sel.is_text_present("exact:ajax_load:"))
 
         sel.type("Password", "xxx")
         sel.click("//input[@name='Check' and @value='3']")
@@ -26,28 +26,28 @@ class FormTestCase(SeleniumTestCase):
         sel.click("submitButton")
         time.sleep(3)
 
-        self.failUnless(sel.is_text_present("exact:ajax_load:"))
-        self.failUnless(sel.is_text_present("exact:Multiple:one"))
-        self.failUnless(sel.is_text_present("exact:Name:MyName1"))
-        self.failUnless(sel.is_text_present("exact:Single2:A"))
-        self.failUnless(sel.is_text_present("exact:Single:one"))
-        self.failUnless(sel.is_text_present("exact:Radio:3"))
-        self.failUnless(sel.is_text_present("exact:Text:This is Form1"))
-        self.failUnless(sel.is_text_present("exact:submitButton:Submit1"))
-        self.failUnless(sel.is_text_present("exact:Hidden:hiddenValue"))
-        self.failUnless(sel.is_text_present("exact:Password:xxx"))
-        self.failUnless(sel.is_text_present("exact:Check:3"))
-        
+        self.assertTrue(sel.is_text_present("exact:ajax_load:"))
+        self.assertTrue(sel.is_text_present("exact:Multiple:one"))
+        self.assertTrue(sel.is_text_present("exact:Name:MyName1"))
+        self.assertTrue(sel.is_text_present("exact:Single2:A"))
+        self.assertTrue(sel.is_text_present("exact:Single:one"))
+        self.assertTrue(sel.is_text_present("exact:Radio:3"))
+        self.assertTrue(sel.is_text_present("exact:Text:This is Form1"))
+        self.assertTrue(sel.is_text_present("exact:submitButton:Submit1"))
+        self.assertTrue(sel.is_text_present("exact:Hidden:hiddenValue"))
+        self.assertTrue(sel.is_text_present("exact:Password:xxx"))
+        self.assertTrue(sel.is_text_present("exact:Check:3"))
+
         # Make sure we can handle other submit methods, and that the
         # value of the submit button is in the request
         sel.click("//input[@name='submitButton' and @value='Submit2']")
         time.sleep(3)
-        self.failUnless(sel.is_text_present("exact:submitButton:Submit2"))
+        self.assertTrue(sel.is_text_present("exact:submitButton:Submit2"))
         sel.click("//button[@name='submitButton']")
         time.sleep(3)
-        self.failUnless(sel.is_text_present("exact:submitButton:Submit5"))
+        self.assertTrue(sel.is_text_present("exact:submitButton:Submit5"))
 
         # pushing submit6 should close the overlay
         sel.click("//input[@name='submitButton6']")
         time.sleep(3)
-        self.failIf(sel.is_text_present("Test Form"))
+        self.assertFalse(sel.is_text_present("Test Form"))
