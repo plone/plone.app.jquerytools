6f981a79dbca8296b724faddb7a09ae26e602945
Johannes Raggam <raggam-nl@adm.at>
2014-06-24 11:07:41 +0200

    shorter functional testing layer

--- plone/app/jquerytools/testing.py
+++ plone/app/jquerytools/testing.py
@@ -2,6 +2,6 @@ from plone.app.testing import PloneSandboxLayer
 from plone.app.testing.layers import FunctionalTesting
 
 
-PLONEAPPJQUERYTPOOLS_FUNCTIONAL_TESTING = FunctionalTesting(
+PAJQT_FUNCTIONAL_TESTING = FunctionalTesting(
     bases=(PloneSandboxLayer(),),
     name="PloneAppJquerytools:Functional")
